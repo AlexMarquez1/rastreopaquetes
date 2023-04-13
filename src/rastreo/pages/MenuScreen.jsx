c19d72e563de76b0b486bd83b4ffffa7c40070a2
Jorge <jorgeluis.gomez.sat@gmail.com>
2023-04-13 11:25:32 -0600

    se agrego h1 de prueba

--- src/rastreo/pages/MenuScreen.jsx
+++ src/rastreo/pages/MenuScreen.jsx
@@ -6,6 +6,7 @@ import { NavBarPrincipal } from '../components/NavBarPrincipal';
 export const MenuScreen = () => {
   return (
     <>
+    <h1>hola</h1>
       {/* <div className="div1">
         <div className="images">
           <img src={fondo}/>
